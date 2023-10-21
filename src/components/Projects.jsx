5aac7ae7fa47d0d5fe8cd127327cd2b1e2c73afa
timworanon <139347732+timworanon@users.noreply.github.com>
2023-10-21 22:37:17 +0700

    edit HTML title and add link for project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,30 +11,34 @@ const Projects = () => {
           <div className="container mt-10 max-w-[1640px] mx-auto bg-[#EDF7FA]">
             <div className='max-w-[1640px] mx-auto p-4 py-8 grid md:grid-cols-3 gap-6'>
                 {/* Cards 1 */}
-              <div className=' rounded-xl relative hover:scale-105 duration-300'>
-                  {/* Overlay */}
-                  <div className='absolute w-full h-full bg-black/30 rounded-xl text-white hover:text-[#ffe564]'>
-                      <p className='font-bold text-2xl px-4 pt-4'>Your Testy</p>
-                      <p className='px-4 font-bold'>The UI web page features famous Thai foods</p>
-                      <p className='px-4 font-bold'>Tools: React/Tailwind/JavaScript</p>
+                <a href="https://thai-food-project.vercel.app/" target='_blank' rel='thai food'>
+                  <div className=' rounded-xl relative hover:scale-105 duration-300'>
+                      {/* Overlay */}
+                      <div className='absolute w-full h-full bg-black/30 rounded-xl text-white hover:text-[#ffe564]'>
+                          <p className='font-bold text-2xl px-4 pt-4'>Your Testy</p>
+                          <p className='px-4 font-bold'>The UI web page features famous Thai foods</p>
+                          <p className='px-4 font-bold'>Tools: React/Tailwind/JavaScript</p>
+                      </div>
+                      <img 
+                      className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+                      src={img1} alt="/" />
                   </div>
-                  <img 
-                  className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
-                  src={img1} alt="/" />
-              </div>
+                </a>
 
                 {/* Cards 2 */}
-                <div className=' rounded-xl relative hover:scale-105 duration-300'>
-                  {/* Overlay */}
-                  <div className='absolute w-full h-full bg-black/30 rounded-xl text-white hover:text-[#ffe564]'>
-                      <p className='font-bold text-2xl px-4 pt-4'>CSS</p>
-                      <p className='px-4 font-bold'>The mini game runs in a console browser</p>
-                      <p className='px-4 font-bold'>Tools: JavaScript</p>
+                <a href="https://replit.com/@parintim345/63-find-your-hat#index.js" target='_blank' rel='find you hat'>
+                  <div className=' rounded-xl relative hover:scale-105 duration-300'>
+                    {/* Overlay */}
+                    <div className='absolute w-full h-full bg-black/30 rounded-xl text-white hover:text-[#ffe564]'>
+                        <p className='font-bold text-2xl px-4 pt-4'>CSS</p>
+                        <p className='px-4 font-bold'>The mini game runs in a console browser</p>
+                        <p className='px-4 font-bold'>Tools: JavaScript</p>
+                    </div>
+                    <img 
+                    className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+                    src={img2} alt="/" />
                   </div>
-                  <img 
-                  className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
-                  src={img2} alt="/" />
-              </div>
+                </a>
             </div>
           </div>
         <div>
